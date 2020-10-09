9115eecddf78ff4ae0dc14629d3ccaf6676363bf
Amaury Soto <amaurysotog@gmail.com>
2020-10-09 18:02:35 -0500

    Javascript para React, everything Vanilla

---
+++ components/user.js
@@ -0,0 +1,18 @@
+import { Component } from "../lib/react.js"
+
+class User extends Component {
+  render() {
+    const { avatar, name } = this.props
+    return `
+    <div class="user">
+      <div class="avatar">
+        <img src="${avatar}" alt="${name} "/>
+      </div>
+      <h2>${name}</h2>
+    </div>
+    
+    `
+  }
+
+}
+export default User
