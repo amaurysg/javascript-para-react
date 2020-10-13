3a7776e17a5f59d5c7e70099f3f9ac89b7007c4e
Amaury Soto <amaurysotog@gmail.com>
2020-10-13 10:23:38 -0500

    Components React

---
+++ components/app.js
@@ -0,0 +1,30 @@
+import { Component } from "../lib/react.js"
+import User from "./user.js"
+import Wrapper from "./wrapper.js"
+
+class App extends Component {
+  render() {
+    return `
+
+    <div class="app">
+    ${new Wrapper({
+      children: `  
+      <h1>Learning React.js 💪🏻👨🏻‍🏫</h1>
+      ${new User({
+        name: "Amaury",
+        avatar: "./images/ash.jpg"
+      }).render()} 
+
+      `
+    }).render()}
+   
+    </div>
+
+    `
+
+  }
+
+
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { render } from "./lib/react-dom.js"
-import User from "./components/user.js"
+/* import User from "./components/user.js" */
+import App from "./components/app.js"
 
 const container = document.querySelector("#root")
 
-render(new User({
-  avatar: "./images/ash.jpg",
-  name: "Amaury",
-}), container)
+render(new App(), container)
