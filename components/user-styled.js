1f44a15f80b57f5e68aad3e6480773678c15d109
Amaury Soto <amaurysotog@gmail.com>
2020-10-20 12:54:36 -0500

    Mode dark

--- components/user-styled.js
+++ components/user-styled.js
@@ -4,7 +4,9 @@ import { Component, createElement } from "../lib/react/index.js"
 
 
 const UserStyled = styled.div`
-  background-image: linear-gradient(to bottom, #f9f9f9 0%, #f9f9f9 130px,rgba(0,0,0,.15) 130px, rgba(0,0,0,.15) 131px, white 131px, white 100%);
+  background-image: linear-gradient(to bottom, ${({ primaryColor }) => primaryColor} 0%, ${({ primaryColor }) => primaryColor} 130px,${({ tertiaryColor }) => tertiaryColor} 130px, ${({ tertiaryColor }) => tertiaryColor} 131px, ${({ secondaryColor }) => secondaryColor} 131px, ${({ secondaryColor }) => secondaryColor} 100%);
+  /* background: ${(props) => props.primaryColor}; */
+  color: ${({ fontColor }) => fontColor} ;
   text-align: center;
   overflow: hidden;
   padding: 20px;
@@ -13,45 +15,67 @@ const UserStyled = styled.div`
   box-shadow: 0 0 1px rgba(0,0,0,.5);
   cursor: pointer;
   user-select: none;
-  color: red; 
-
 `
 
 const AvatarStyled = styled.img`
   max-width: 150px;
-  border-radius: 25%;
+  border-radius: 50%;
   border: 5px solid white;
   box-shadow: 0 0 2px black;
 `
 
-class User extends Component {
-  render() {
 
+const theme = {
+  light: {
+    primaryColor: '#f9f9f9',
+    secondaryColor: 'white',
+    tertiaryColor: 'rgba(0,0,0,.15)',
+    fontColor: 'black',
+  },
+  dark: {
+    primaryColor: '#212429',
+    secondaryColor: '#212429',
+    tertiaryColor: 'white',
+    fontColor: 'white',
+  }
+}
+
+
+class User extends Component {
+  state = {
+    mode: 'light'
+  }
+  setMode = (event) => {
+    // console.log(event.matches)
+    if (event.matches) {
+      return this.setState({
+        mode: 'dark'
+      })
+    }
+    this.setState({
+      mode: 'light'
+    })
+  }
+  componentDidMount() {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    this.setMode(mediaQuery)
+    mediaQuery.addEventListener('change', this.setMode)
+  }
+  render() {
+    const { mode } = this.state
+    const colors = (mode === 'light') ? theme.light : theme.dark
     const { name, avatar } = this.props
+    console.log(colors)
     return UserStyled({
+      ...colors,
       children: [
         AvatarStyled({
           src: avatar
-        }), createElement("h2", null, name)
+        }),
+        createElement('h2', null, `${name} ${mode} mode`)
       ]
-    }, "")
-
-
-
-
-
-    /*    `
-     
-     ${UserStyled(`
-       ${AvatarStyled(`   
-     src= ${avatar}
-     `)}
-     <h2> ${name} </h2>
-     `)}
-     
-     ` */
+    }, '')
   }
-
 }
 
 export default User
